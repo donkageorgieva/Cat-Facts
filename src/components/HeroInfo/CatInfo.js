64692a6093105e1978aec50c72e8063bb2b01d65
Donka Georgieva <DoniGeorgieva16@gmail.com>
2021-04-01 13:34:55 +0300

    Adding Framer-Motion to the Header

--- src/components/HeroInfo/CatInfo.js
+++ src/components/HeroInfo/CatInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import styles from "./CatInfo.module.scss";
 import { scroller } from "react-scroll";
+import {motion} from 'framer-motion';
 
 const catInfo = (props) => {
   const smoothScroll = (href) => {
@@ -11,6 +12,60 @@ const catInfo = (props) => {
       duration: 500,
     });
   };
+  const scrollLeft = {
+    hidden: {
+      opacity: 0,
+      x: -100,
+    },
+    visible: {
+      opacity: 1,
+      transition: 0.65,
+      x: 0,
+
+
+    }
+  }
+  const scrollTop = {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      transition: 0.65,
+      y: 0,
+
+
+    }
+  }
+  const scrollBottom = {
+    hidden: {
+      opacity: 0,
+      y: 100,
+    },
+    visible: {
+      opacity: 1,
+      transition: 0.65,
+      y: 0,
+
+
+    }
+    
+  }
+  const scrollRight ={
+    hidden: {
+      opacity: 0,
+      x: 100,
+    },
+    visible: {
+      opacity: 1,
+      transition: 0.65,
+      x: 0,
+
+
+    }
+  }
+  
   return (
     <section
       className={[styles.CatInfo, "d-flex align-items-center home"].join(" ")}
@@ -18,41 +73,43 @@ const catInfo = (props) => {
       <Container className={styles.HeroContainer}>
         <Row>
           <Col sm className={styles.HeroTextContainer}>
-            <h1> Cats?</h1>
-            <h2>
+            <motion.h1 initial="hidden" animate="visible" variants={scrollLeft}> Cats?</motion.h1>
+            <motion.h2 initial="hidden" animate="visible" variants={scrollLeft}>
               Find out information about any cat breed in just a single click!{" "}
-            </h2>
-            <p className="pt-2">
+            </motion.h2>
+            <motion.p initial="hidden" animate="visible" variants={scrollLeft} className="pt-2">
               Are you a future cat owner looking to adopt or purchase the
               perfect cat for your lifestyle? Or are you just a huge cat geek?
               It doesn't matter! Find out the cat info you was looking for!
-            </p>
+            </motion.p>
             <Col>
               <Row>
-                <Button
-                  variant="primary"
+                <motion.button initial="hidden"
+                 animate="visible" variants={scrollBottom}
+                  className="mr-3 btn btn-primary"
                   onClick={() => {
                     smoothScroll("search-cats");
                   }}
-                  className="mr-3"
+                 
                 >
                   Start Now
-                </Button>
+                </motion.button>
 
-                <Button
-                  variant="outline-info"
+                <motion.button className="btn btn-outline-info"
+                initial="hidden" animate="visible" variants={scrollBottom}
+                
                   onClick={() => {
                     smoothScroll("contact");
                   }}
                 >
                   Contacts
-                </Button>
+                </motion.button>
               </Row>
             </Col>
           </Col>
 
           <Col sm className={styles.HeroImageContainer}>
-            <div className={styles.HeroImage}></div>
+            <motion.div className={styles.HeroImage} initial="hidden" animate="visible" variants={scrollRight}></motion.div>
           </Col>
         </Row>
       </Container>
